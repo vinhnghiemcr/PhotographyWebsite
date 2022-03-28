3636d641673d428eab692100964dfd71cda9b7fc
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-28 14:02:10 -0500

    create server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.send('This is root!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server listening on port ${PORT}`)
+})
