dc1ddf486a9bd31fab45eae809dbc4f75f7db6ea
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-28 12:22:29 -0500

    Create Package schema

---
+++ models/Package.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose')
+
+const Package = new Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true},
+    service: [{ type: Schema.Types.ObjectId, ref: 'Service'}]
+  },
+  { timestamps: true }
+)
+
+module.exports = Package
